762e159ee8f3d6edfafbb1d2d6ab54da996d050b
Vinayak Gore <137513396+VinuGore26@users.noreply.github.com>
2023-09-22 02:23:49 +0530

    Add files via upload
    Adding files with the updation in logic for snake movement with the sound for each key.

--- SnakeGame/js/index.js
+++ SnakeGame/js/index.js
@@ -12,7 +12,7 @@ food = { x: 13, y: 10 };
 // Game Functions
 function main(ctime) {
   window.requestAnimationFrame(main);
-  console.log(ctime);
+  // console.log(ctime);
   if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
     return;
   }
@@ -20,8 +20,26 @@ function main(ctime) {
   gameEngine();
 }
 
+function isCollide(sarr){
+  return false;
+}
+
 function gameEngine() {
   // part 1 : Updating the snakearr & food
+  if(isCollide(snakeArr)){
+    gameOverSound.play();
+    musicSound.pause();
+    inputDir = { x: 0, y: 0};
+    alert("Game Over. Press any key to play again!");
+    snakeArr = [{x:13 , y:15}];
+    musicSound.play();
+    score = 0;
+  }
+
+  // If you have eaten the food, increment the score and regenrate the food
+  if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+    snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
+  }
 
   // part 2 : Display the snake & food
   // Display the snake
@@ -48,3 +66,32 @@ function gameEngine() {
 
 // Main logic starts from here
 window.requestAnimationFrame(main);
+window.addEventListener("keydown", (e) => {
+  inputDir = { x: 0, y: 1 }; // start the game
+  moveSound.play();
+  switch (e.key) {
+    case "ArrowUp":
+      console.log("ArrowUp");
+      input.x = 0;
+      input.y = 1;
+      break;
+    case "ArrowDown":
+      console.log("ArrowDown");
+      input.x = 0;
+      input.y = -1;
+      break;
+    case "ArrowLeft":
+      console.log("ArrowLeft");
+      input.x = -1;
+      input.y = 0;
+      break;
+    case "ArrowRight":
+      console.log("ArrowRight");
+      input.x = 1;
+      input.y = 0;
+      break;
+
+    default:
+      break;
+  }
+});
